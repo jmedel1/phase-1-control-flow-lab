ef02dac2ffc9dc98e75f41d3299fafbcec7cf702
jael <jael.medel30@gmail.com>
2023-03-15 20:21:13 -0500

    control flow

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(x) {
   // Write your code here!
+if (x <= 400) {
+  return 'This one is on me!';
+}
+else if (x > 400 && x <= 2000) {
+  return 'That will be twenty bucks.';
+}
+else if (x > 2000 && x <= 2500) {
+  return 'I will gladly take your thirty bucks.';
+}
+else {
+  return 'No can do.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === "NYC") {
+    return "Ok, sounds good.";
 }
+else{
+    return "No go.";
+}
+}
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  if (tip === "generous") {
+    return "Thank you so much.";
+  }
+  else if (tip === "not as generous") {
+    return "Thank you.";
+   }
+    else {
+    return "Bye.";
+    }
+  }
